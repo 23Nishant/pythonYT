a443befbf53236fc9090aa6d71e38d5dfd86caf3
Nishant Tomar <108018788+23Nishant@users.noreply.github.com>
2022-12-14 10:17:16 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,32 @@
+a = 34
+b = 32
+# Arithmetic operators
+print('Arithmetic Operators')
+print("The value of a + b is:", a+b)
+print("The value of a * b is:", a*b)
+print("The value of a - b is:", a-b)
+print("The value of a / b is:", a/b) # / gives us the divident
+print("The value of a % b is:", a%b) # % gives us the remainder
+
+# Assignment Operators
+print('Assignment Operators')
+a += 12
+print(a)
+b -= 22
+print(b)
+
+# other operators are %= , /= , *=
+
+#Comparison Operators
+print('Comparison Operators')
+print(a>b)
+print(a==b)
+print(a>=b)
+print(a<=b)
+print(a!=b)
+
+#Logical Operators
+print('Logical Operators')
+print(a and b)
+print(a or b)
+print(not b)
