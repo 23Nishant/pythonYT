1353938b1866dc578bec410db18fa3a062629fa9
Nishant Tomar <108018788+23Nishant@users.noreply.github.com>
2022-12-14 09:59:08 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,25 @@
+# can not start a variable name with a number but can start with underscore _
+a = "Nishant"
+print(a)
+_a1 = "Nishant Tomar"
+print(_a1)
+#case sensitive 
+A = 'Nishant Tomar'
+print(A)
+b = 23
+print(b)
+c = 23.23
+print(c)
+d = True
+print(d)
+#used triple quotes to print the string as I am using single quote in the sentence
+# triple quotes also lets me print in multiple linea
+e = '''Nishant's
+Python
+Program''' 
+print(e)
+f = None
+print(f)
+# searching the type of the variable
+print(type(a))
+print(type(f))
